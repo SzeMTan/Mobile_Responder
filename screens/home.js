203ff9e1de2da31c380117bdfb3ce0fe97ee9337
JennyLee <0608jennylee@hotmail.co.nz>
2019-07-04 16:14:16 +1200

    added card to home screen

--- screens/home.js
+++ screens/home.js
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
-import { StyleSheet, View } from 'react-native';
+import { ScrollView, StyleSheet, View } from 'react-native';
 import { Header, Text, ThemeProvider } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
-import ConsumerComponent from '../components/segcomponent'
+import ConsumerComponent from '../components/segcomponent';
+import {Card, CardImage, CardTitle, CardContent, CardButton, CardAction} from 'react-native-cards';
 
 export default class Home extends Component {
     render() {
         return (
             <View>
                 <Header
-                    centerComponent={{ text: 'HOME', style: { fontSize:24 } }}
+                    centerComponent={{ text: 'HOMES', style: { fontSize:24 } }}
                     backgroundColor='none'
                 />
                 <ConsumerComponent/>
+                <ScrollView>
+                    <Card>
+                        <CardTitle
+                        title="Unit Status Code: "
+                        />                        
+                    </Card>
+                    <Card>
+                        <CardTitle
+                        title="Unit: "
+                        />
+                        <CardContent text="Name: " />
+                        <CardContent text="QIDs: " />
+                        <CardContent text="ph. no: " />
+                        <CardContent text="Shift start: " />
+                        <CardContent text="Shift end: " />
+                        <CardContent text="radio no: " />
+                    </Card>
+                </ScrollView>
             </View>
         );
     }

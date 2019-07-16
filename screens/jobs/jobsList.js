9b83a30a7174a137f7414c5d626768a3db0683e8
JennyLee <0608jennylee@hotmail.co.nz>
2019-07-16 16:39:05 +1200

    added data to jobs

--- screens/jobs/jobsList.js
+++ screens/jobs/jobsList.js
@@ -1,14 +1,58 @@
-import React, { Component } from "react";
-import { View } from 'react-native';
+import React, { Alert, Component } from "react";
+import { ScrollView, View, TouchableOpacity } from 'react-native';
 import HeaderComponent from '../../components/customHeaderComponent';
+import SearchBarComponent from '../../components/customSearchBarComponent';
+import CardComponent from '../../components/customCardComponent';
 
 export default class JobsList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+          {title: 'P036986219',
+            priority: 'P2',
+            code: '3C-CRIME PREVENTION ADVICE',
+            destination: '10 Waterloo Quadrant',
+            date: '17/03/19 5:35PM',
+            status: 'PENDING',
+        },
+        {
+            title: 'P036986218',
+            priority: 'P2',
+            code: '3C-CRIME PREVENTION ADVICE',
+            destination: '10 Waterloo Quadrant',
+            date: '17/03/19 5:35PM',
+            status: 'PENDING',
+        },
+        {
+            title: 'P036986217',
+            priority: 'P2',
+            code: '3C-CRIME PREVENTION ADVICE',
+            destination: '10 Waterloo Quadrant',
+            date: '17/03/19 5:35PM',
+            status: 'PENDING',
+        },
+    ],};
+  }
 
-    render() {
-        return (
-            <View>
-                <HeaderComponent title={'Jobs()'}/>
-            </View>
-        );
-    }
+  render() {
+    const cards = this.state.data.map(job =>         
+    <TouchableOpacity key={job.title} onPress={() => console.log("hello")}>
+      <CardComponent key={job.title} 
+      title={job.title} 
+      titlecontent={[job.code, job.destination]}
+      leftbottom={job.date} rightbottom={job.status}    
+      
+      /></TouchableOpacity>)
+    return (
+        <View>
+            <SearchBarComponent title='Jobs'/>
+            <ScrollView >
+                <View style={{flex:1,justifyContent:'center'}}>
+                    {cards}
+                </View>
+            </ScrollView>
+        </View>
+    );
+  }
 }
--- screens/units/unitsList.js
+++ screens/units/unitsList.js
@@ -1,4 +1,4 @@
-import React, { Alert, Component } from "react";
+import React, { Component } from "react";
 import { ScrollView, View, TouchableOpacity } from 'react-native';
 import HeaderComponent from '../../components/customHeaderComponent';
 import SearchBarComponent from '../../components/customSearchBarComponent';
@@ -48,22 +48,24 @@ export default class UnitsList extends Component {
   };
 
   render() {
-    const cards = this.state.data.map(unit =>         
-    <TouchableOpacity key={unit.title} onPress={() => 
-    this.props.navigation.navigate('IndividualUnit', {id: 1, title: unit.title})}>
-      <CardComponent key={unit.title} 
-      title={unit.title} 
-      titlecontent={[unit.code, unit.destination]}
-      leftbottom={unit.id} rightbottom={unit.status}    
-      onPress={() => {Alert.alert('hello')}}
-      /></TouchableOpacity>)
+    const cards = this.state.data.map(
+      unit =>         
+        <TouchableOpacity key={unit.title} onPress={() => 
+        this.props.navigation.navigate('IndividualUnit', {id: 1, title: unit.title})}>
+          <CardComponent key={unit.title} 
+          title={unit.title} 
+          titlecontent={[unit.code, unit.destination]}
+          leftbottom={unit.id} rightbottom={unit.status}
+          />
+        </TouchableOpacity>
+      )
     return (
         <View>
             <SearchBarComponent title='Units'/>
             <ScrollView >
-            <View style={{flex:1,justifyContent:'center'}}>
-            {cards}
-            </View>
+              <View style={{flex:1,justifyContent:'center'}}>
+                {cards}
+              </View>
             </ScrollView>
         </View>
     );

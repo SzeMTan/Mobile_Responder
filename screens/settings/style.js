7af8b9c7e2664f1fa7b1bafe405ef378a9f491d9
SzeMTan <stan557@aucklanduni.ac.nz>
2019-07-21 21:13:25 +1200

    refactor styles for settings

---
+++ screens/settings/style.js
@@ -0,0 +1,15 @@
+import {StyleSheet} from "react-native"
+
+export default settingsStyles = StyleSheet.create({
+    emergencyButton: {
+        flexDirection: 'column',
+        backgroundColor: 'red',
+        height: 100,
+        marginHorizontal: 10,
+      },
+      container:{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+      },
+    })

fb22b41c53d2390de08a59c37a7ebcfd0fc9309e
SzeMTan <stan557@aucklanduni.ac.nz>
2019-08-12 10:53:56 +1200

    global job list

--- global.js
+++ global.js
@@ -1,4 +1,35 @@
 module.exports = {
     darkState: false,
+    jobs: [
+        {title: 'P036986219',
+          priority: 'P2',
+          code: '3C-CRIME PREVENTION ADVICE',
+          destination: '10 Waterloo Quadrant',
+          date: '17/03/19 5:35PM',
+          status: 'PENDING',
+          latlng: {latitude: -36.848671,
+            longitude: 174.770107}
+      },
+      {
+          title: 'P036986218',
+          priority: 'P2',
+          code: '3C-CRIME PREVENTION ADVICE',
+          destination: '10 Waterloo Quadrant',
+          date: '17/03/19 5:35PM',
+          status: 'PENDING',
+          latlng: {latitude: -36.853943,
+            longitude: 174.768265}
+      },
+      {
+          title: 'P036986217',
+          priority: 'P2',
+          code: '3C-CRIME PREVENTION ADVICE',
+          destination: '10 Waterloo Quadrant',
+          date: '17/03/19 5:35PM',
+          status: 'PENDING',
+          latlng: {latitude: -36.852638,
+            longitude: 174.768265}
+      },
+  ]
  };
  
--- screens/jobs/jobsList.js
+++ screens/jobs/jobsList.js
@@ -16,37 +16,7 @@ export default class JobsList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: [
-          {title: 'P036986219',
-            priority: 'P2',
-            code: '3C-CRIME PREVENTION ADVICE',
-            destination: '10 Waterloo Quadrant',
-            date: '17/03/19 5:35PM',
-            status: 'PENDING',
-            latlng: {latitude: -36.848671,
-              longitude: 174.770107}
-        },
-        {
-            title: 'P036986218',
-            priority: 'P2',
-            code: '3C-CRIME PREVENTION ADVICE',
-            destination: '10 Waterloo Quadrant',
-            date: '17/03/19 5:35PM',
-            status: 'PENDING',
-            latlng: {latitude: -36.853943,
-              longitude: 174.768265}
-        },
-        {
-            title: 'P036986217',
-            priority: 'P2',
-            code: '3C-CRIME PREVENTION ADVICE',
-            destination: '10 Waterloo Quadrant',
-            date: '17/03/19 5:35PM',
-            status: 'PENDING',
-            latlng: {latitude: -36.852638,
-              longitude: 174.768265}
-        },
-    ],};
+  };
   }
 
   static navigationOptions = ({ navigation }) => {
@@ -63,7 +33,7 @@ export default class JobsList extends Component {
   }
 
   _JobsListSize = () => {
-    return this.state.data.length;
+    return GLOBAL.jobs.length;
   };
 
   newJob = () => {
@@ -71,7 +41,7 @@ export default class JobsList extends Component {
   }
 
   render() {
-    const cards = this.state.data.map(job =>         
+    const cards = GLOBAL.jobs.map(job =>         
     <TouchableOpacity key={job.title} 
     onPress={() => 
     this.props.navigation.navigate('IndividualJob', {id: 1, title: job.title, code: job.code, date: job.date, status: job.status, priority: job.priority, latlng: job.latlng})}>
